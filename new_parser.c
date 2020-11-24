666a9c2a80afd38c03cc45fc9c0c0acf0b32052c
TS-N <2501@holbertonschool.com>
2020-11-24 09:07:18 +0000

    frees nicely and allow new commands to be inouted

--- new_parser.c
+++ new_parser.c
@@ -8,10 +8,10 @@ int	add_cmd_lst(char *cmd, cmd_lst_t **head, char flag)
 	node = (cmd_lst_t *)malloc(sizeof(cmd_lst_t));
 	if (!node)
 		return (-1);
-	printf("In add_cmd_lst cmd = [%s]\n", cmd);
+	//printf("In add_cmd_lst cmd = [%s]\n", cmd);
 	node->cmd = cmd;
 	node->av = strtoav(cmd, " \t");
-	printf("In add_cmd_lst av[0] = [%s]\n", node->av[0]);
+	//printf("In add_cmd_lst av[0] = [%s]\n", node->av[0]);
 	node->exe = 0;
 	node->flag = flag;
 	node->next = NULL;
@@ -37,9 +37,11 @@ int	add_cmd_lst_lst(char *list, cmd_lst_lst_t **head)
 	cmd_lst_lst_t	*buf;
 
 	node = (cmd_lst_lst_t *)malloc(sizeof(cmd_lst_lst_t));
+	//printf("In add_cms_lst_lst node (%p)\n", node);
+	//printf("In add_cmd_lst_lst *head (%p)\n", *head);
 	if (!node)
 		return (-1);
-	printf("In add_cmd_lst_lst list = [%s]\n", list);
+	//printf("In add_cmd_lst_lst list = [%s]\n", list);
 	node->list = list;
 	node->head = NULL;
 	node->next = NULL;
@@ -61,13 +63,13 @@ int	parse_logic_op(cmd_lst_lst_t **llav_head) // create the list of char**av -> 
 	char		*list;
 	int		i;
 
-	printf("llav_head (%p) *(%p)\n", llav_head, *llav_head);
+	//printf("In parse logic op llav_head (%p) *(%p)\n", llav_head, *llav_head);
 	if (!llav_head)
 		return (-1);
 	node = *llav_head;
 	while (node)
 	{
-		printf("In parse_logic loop\n");
+		//printf("In parse_logic loop\n node (%p) \nnode->list = [%s]\n", node, node->list);
 		list = node->list;
 		strsplit(node, node->list);
 		node = node->next;
@@ -86,6 +88,7 @@ int	parse_ctrl_op(char *input, cmd_lst_lst_t **head) // create the list of char 
 		return (-1);
 	while (lists[i])
 	{
+		//printf("in parse op lists[%d] (%p) = [%s]\n", i, lists[i], lists[i]);
 		add_cmd_lst_lst(lists[i], head);
 		++i;
 	}
@@ -98,31 +101,32 @@ int	parser(char *input, cmd_lst_lst_t **llav_head)
 		return (-1);
 	if (parse_ctrl_op(input, llav_head) == -1)
 		return (-1);
+	//printf("Parce_strl_op passed\n");
 	if (parse_logic_op(llav_head) == -1)
 		return (-1);
+	//printf("Parse_logic_op passed\n");
 
-	/* tests*/ 
+	/* tests
 	cmd_lst_t	*node;
 	cmd_lst_lst_t	*head;
 	int i =0, j = 0;
 	head = *llav_head;
 	while(head)
 	{
-		printf("llav_node(%d): [%s]\n", i, head->list);
-		printf("llav_head->head: (%p)\n", head->head);
+		//printf("llav_node(%d): [%s]\n", i, head->list);
+		//printf("llav_head->head: (%p)\n", head->head);
 		node = (head->head);
-		printf("NOPE\n");
 		j = 0;
 		while (node)
 		{
-			printf("node (%d) cmds: [%s] av: [%p] flag: [%c]\n",j, node->cmd,node->av, node->flag);
+			printf("In parser node (%d) cmds: [%s] av: [%p] flag: [%c]\n",j, node->cmd,node->av, node->flag);
 			node = node->next;
 			++j;
 		}
 		head = (head->next);
 		++i;
 	}
-	 /*end tests */
+	 end tests */
 	return (1);
 }
 /*
